069f5150818a844996497a9947ffe9322e402ec4
RihoYokoi <81666623+RihoYokoi@users.noreply.github.com>
2021-06-30 20:27:32 +0700

    clean(public folder): Refactoring code
    - index.html
    - detailpost.html
    - editpost.html
    - newpost.html
    
    Closes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://127.0.0.1:5500'
+    origins 'http://127.0.0.1:5500','http://127.0.0.1:3001'
 
     resource '*',
       headers: :any,
--- test/controllers/comment_posts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CommentPostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
